25b9ba9574dae42f8892d6a1021d3941262823e8
Dhruv Jain <dhruvjain136@gmail.com>
2022-07-20 17:58:45 +0530

    Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:trendz_app/models/theme.dart';
 
 import 'widgets/slider.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
---
+++ lib/services/auth.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import '../models/user.dart';
+import 'package:twitter_login/twitter_login.dart';
+
+class AuthServices {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  MyUser? _userFromFirebaseUser(User? user) {
+    return user != null ? MyUser(user.uid) : null;
+  }
+
+  Stream<MyUser?> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+  }
+
+  // Login
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  //Sign In
+  Future registerWithEmailAndPassword(
+      String email, String password, String username) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      await user!.updateDisplayName(username);
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // Sign Out
+  Future signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  ////Twitter sigin
+  // Future<UserCredential> signInWithTwitter() async {
+  //   // Create a TwitterLogin instance
+  //   final twitterLogin = new TwitterLogin(
+  //       apiKey: '<your consumer key>',
+  //       apiSecretKey: ' <your consumer secret>',
+  //       redirectURI: '<your_scheme>://');
+
+  //   // Trigger the sign-in flow
+  //   final authResult = await twitterLogin.login();
+
+  //   // Create a credential from the access token
+  //   final twitterAuthCredential = TwitterAuthProvider.credential(
+  //     accessToken: authResult.authToken!,
+  //     secret: authResult.authTokenSecret!,
+  //   );
+
+  //   // Once signed in, return the UserCredential
+  //   return await FirebaseAuth.instance
+  //       .signInWithCredential(twitterAuthCredential);
+  // }
+}
